440682d20962c5c35ede641efc9c86dc6164252b
Irina Romjalis <iromjalis@gmail.com>
2021-04-03 21:44:44 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import './styles.css';
-
-// refs = {
-//     container: document.querySelector('.container'),
-//     input: document.querySelector('.input')
-// }
-
-// refs.input.addEventListener('input', e=>{
-//     console.log(e.currentTarget.value);
-// })
-
-
-// debounce(x => {
-//     console.log('hello world!');
-//   }, 100)
+import './js/template.js';
+import './js/changingTheme.js';
