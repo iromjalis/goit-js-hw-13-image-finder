80da875515a0b1ac8bdbb7ff6894b3320b5fa51a
Irina Romjalis <iromjalis@gmail.com>
2021-04-23 23:22:00 +0200

    +

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+  const refs = {
+    searchForm : document.querySelector('.js-search-form'),
+    articlesContainer : document.querySelector('.js-articles') ,
+    galleryListRef : document.querySelector('.js-gallery') ,
+    modalImgRef : document.querySelector('.lightbox__image') ,
+    modalRef : document.querySelector('.lightbox'),
+    loadMore: document.querySelector('.button')
+}
+
+export default refs
