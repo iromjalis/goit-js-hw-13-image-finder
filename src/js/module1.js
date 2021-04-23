84caf1c3b7efea3b54f41a5ac820ab76189842d6
Irina Romjalis <iromjalis@gmail.com>
2021-04-24 01:32:20 +0200

    +

--- src/js/module1.js
+++ src/js/module1.js
@@ -88,5 +88,15 @@ const max = 10;
 // }
 // console.log('Hello');
 
+// const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
+// const max = colors.length - 1;
+// const min = 0;
 
+// const index = Math.round(Math.random() * (max - min) + min);
+
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
 
