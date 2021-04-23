a14f64ef5a2f12ad57050fe5cb7567177de3bb9e
Irina Romjalis <iromjalis@gmail.com>
2021-04-23 21:22:05 +0200

    +

---
+++ src/js/module1.js
@@ -0,0 +1,25 @@
+//*Statement - иструкция
+
+const b =10
+const a=b*2;
+// console.log(a); //~20
+
+//*Expressio - выражение
+
+// [[a] = [[b] * [2]]]
+
+//*Свойство - существителное
+
+'JavaScript is awesome'.length
+console.log('JavaScript is awesome'.length); //~21 /рост/вес/
+
+//*Метод - действие
+console.log('JavaScript is awesome'.toUpperCase()) 
+//~JAVASCRIPT IS AWESOME
+
+//*Идентификатор = имя переменной, функции, параметра, класса
+const _lovercase = 10;
+const $dollar = 5;
+
+//* Prompt + Confirm
+const message = 'JavaScript is awesome';
